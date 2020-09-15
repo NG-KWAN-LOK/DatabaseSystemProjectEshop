221b2fe5e2d2384e08e939a954bb115da6164f96
NG-KWAN-LOK <t106590052@ntut.org.tw>
2020-09-15 23:51:59 +0800

    no message

--- web/connect.php
+++ web/connect.php
@@ -1,9 +1,9 @@
 <?php
 
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "";
- $db = "dvd";
+ $dbhost = "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+ $dbuser = "h3yp8cch3p9mp3q8";
+ $dbpass = "q3ry8fhbb75w71y9";
+ $db = "qkurcgqzk3zfs0bv";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed:");
  $conn->query("SET NAMES 'UTF8'");
 
